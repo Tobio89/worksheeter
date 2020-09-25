5d7d6b2b29fa63859a90383669e9f35b1ea5083a
Tobias S <44494591+BruceJi7@users.noreply.github.com>
2020-09-25 21:50:34 +0900

    add vue.js to index page

---
+++ app/static/wsm_vue.js
@@ -0,0 +1,17 @@
+console.log('WSM_VUE FILE WAS LOADED')
+console.log('WE GON MAKE THIS SITE VUETIFUL well a little anyway')
+
+
+let sheet_instructions = new Vue({
+    el: '#instructions',
+    delimiters: ['%[', ']%'],
+    data: {
+        main_shown:false,
+        read_shown:false,
+        list_shown:false,
+    },
+    methods:{},
+    computed:{}
+})
+
+
